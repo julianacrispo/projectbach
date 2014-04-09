4e278d9f7cb971d0ec12aa5e9ac2ef88433b61ce
juliana crispo <julianacrispo@gmail.com>
2014-04-09 19:19:55 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bachelorette::Application.config.secret_key_base = 'aac4bd47f2155b929f0fe41d6e56e97cd3ff4220bec61e3b9aa657b800c9b1045aa1d4d19109cddec8d9e8c8f0085694bc6b0d798afecfe69a839596118c64ed'
